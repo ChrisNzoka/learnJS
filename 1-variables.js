11a1f3ca6f5d96b19a106e87353660150491ad38
Chris Nzoka-okoye <chrisnzokaokoye@gmail.com>
2022-12-14 06:09:52 +0100

    created a task on variables

---
+++ 1-variables.js
@@ -0,0 +1,19 @@
+/*
+To create or declare a variable in JavaScript, we'll put the keyword var before  the variable as below
+*/
+
+var myVariable;
+
+/*
+Variable names can be made up of numbers, letters, and $ or _, but may not contain spaces or start with a number.
+*/
+
+// Further, we can store a value in a variable with the assignment operator (=)
+
+myVariable = 84;
+
+// Next, we can assig the value of one variable to another vriable
+
+var newVariable;
+newVariable = myVariable;
+console.log(newVariable); //This will display 84 as the valu of newVariable.
